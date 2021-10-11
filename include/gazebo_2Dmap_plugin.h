349e2c22135a8920aa67072c4b1bd8b423de325e
Jacenty00 <j.kmiecik@campus.tu-berlin.de>
2021-10-11 12:13:19 +0200

    Layered height occupancy map

--- include/gazebo_2Dmap_plugin.h
+++ include/gazebo_2Dmap_plugin.h
@@ -36,7 +36,8 @@
 #include <costmap_2d/costmap_2d_ros.h>
 #include <std_srvs/Empty.h>
 
-namespace gazebo {
+namespace gazebo
+{
 
 #if GAZEBO_MAJOR_VERSION >= 9
   typedef ignition::math::Vector3d vector3d;
@@ -46,73 +47,73 @@ namespace gazebo {
   auto GetPhysicsPtr = std::mem_fn(&gazebo::physics::World::GetPhysicsEngine);
 #endif
 
-/// \brief    Octomap plugin for Gazebo.
-/// \details  This plugin is dependent on ROS, and is not built if NO_ROS=TRUE is provided to
-///           CMakeLists.txt. The PX4/Firmware build does not build this file.
-class OccupancyMapFromWorld : public WorldPlugin {
- public:
-  OccupancyMapFromWorld()
-      : WorldPlugin(), name_("gazebo_2Dmap_plugin")
+  /// \brief    Octomap plugin for Gazebo.
+  /// \details  This plugin is dependent on ROS, and is not built if NO_ROS=TRUE is provided to
+  ///           CMakeLists.txt. The PX4/Firmware build does not build this file.
+  class OccupancyMapFromWorld : public WorldPlugin
   {
-    ROS_INFO_NAMED(name_, "occupancy map plugin started");
-  }
-  virtual ~OccupancyMapFromWorld();
+  public:
+    OccupancyMapFromWorld()
+        : WorldPlugin(), name_("gazebo_2Dmap_plugin")
+    {
+      ROS_INFO_NAMED(name_, "occupancy map plugin started");
+    }
+    virtual ~OccupancyMapFromWorld();
 
+  protected:
+    /// \brief Load the plugin.
+    /// \param[in] _parent Pointer to the world that loaded this plugin.
+    /// \param[in] _sdf SDF element that describes the plugin.
+    void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf);
 
+    bool worldCellIntersection(const vector3d &cell_center, const double cell_length,
+                               gazebo::physics::RayShapePtr ray);
 
- protected:
+    //  void FloodFill(const math::Vector3& seed_point,
+    //                 const math::Vector3& bounding_box_origin,
+    //                 const math::Vector3& bounding_box_lengths,
+    //                 const double leaf_size);
 
-  /// \brief Load the plugin.
-  /// \param[in] _parent Pointer to the world that loaded this plugin.
-  /// \param[in] _sdf SDF element that describes the plugin.
-  void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf);
-
-  bool worldCellIntersection(const vector3d& cell_center, const double cell_length,
-                              gazebo::physics::RayShapePtr ray);
-  
-
-//  void FloodFill(const math::Vector3& seed_point,
-//                 const math::Vector3& bounding_box_origin,
-//                 const math::Vector3& bounding_box_lengths,
-//                 const double leaf_size);
-  
-  /*! \brief
+    /*! \brief
   */
-  void CreateOccupancyMap();
+    void CreateOccupancyMap();
 
-  static void cell2world(unsigned int cell_x, unsigned int cell_y,
-                         double map_size_x, double map_size_y, double map_resolution,
-                         double& world_x, double &world_y);
+    static void cell2world(unsigned int cell_x, unsigned int cell_y,
+                           double map_size_x, double map_size_y, double map_resolution,
+                           double &world_x, double &world_y);
 
-  static void world2cell(double world_x, double world_y,
-                         double map_size_x, double map_size_y, double map_resolution,
-                         unsigned int& cell_x, unsigned int& cell_y);
+    static void world2cell(double world_x, double world_y,
+                           double map_size_x, double map_size_y, double map_resolution,
+                           unsigned int &cell_x, unsigned int &cell_y);
 
-  static bool cell2index(int cell_x, int cell_y,
-                         unsigned int cell_size_x, unsigned int cell_size_y,
-                         unsigned int& map_index);
+    static bool cell2index(int cell_x, int cell_y,
+                           unsigned int cell_size_x, unsigned int cell_size_y,
+                           unsigned int &map_index);
 
-  static bool index2cell(int index, unsigned int cell_size_x, unsigned int cell_size_y,
-                         unsigned int& cell_x, unsigned int& cell_y);
+    static bool index2cell(int index, unsigned int cell_size_x, unsigned int cell_size_y,
+                           unsigned int &cell_x, unsigned int &cell_y);
 
- private:
-  bool ServiceCallback(std_srvs::Empty::Request& req,
-                       std_srvs::Empty::Response& res);
+  private:
+    bool ServiceCallback(std_srvs::Empty::Request &req,
+                         std_srvs::Empty::Response &res);
 
-  physics::WorldPtr world_;
-  ros::NodeHandle nh_;
-  ros::ServiceServer map_service_;
-  ros::Publisher map_pub_;
-  nav_msgs::OccupancyGrid* occupancy_map_;
-  std::string name_;
-  double map_resolution_;
-  double map_height_;
-  double map_size_x_;
-  double map_size_y_;
-  double init_robot_x_;
-  double init_robot_y_;
-};
+    physics::WorldPtr world_;
+    ros::NodeHandle nh_;
+    ros::ServiceServer map_service_;
+    ros::Publisher map_pub_;
+    nav_msgs::OccupancyGrid *occupancy_map_;
+    std::string name_;
+    double map_resolution_;
+    double map_height_;
+    double map_size_x_;
+    double map_size_y_;
+    double init_robot_x_;
+    double init_robot_y_;
+    bool check_below;
+    double step;
+    double lower_bound;
+  };
 
 } // namespace gazebo
 
-#endif  // GAZEBO_2DMAP_PLUGIN_H
+#endif // GAZEBO_2DMAP_PLUGIN_H
